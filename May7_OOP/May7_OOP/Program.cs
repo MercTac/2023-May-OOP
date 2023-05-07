e757db8b0fc2f497058d5e85e2add62bf2403fc9
Mykyta Khmelevskyi <126295194+MercTac@users.noreply.github.com>
2023-05-07 20:00:29 +0200

    Add files via upload

---
+++ May7_OOP/May7_OOP/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace May7_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first num: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter second num: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter third num: ");
+            double z = Convert.ToDouble(Console.ReadLine());
+            double r = (x + y + z) / 3;
+            Console.WriteLine("result: " + r);
+            Console.ReadLine();
+        }
+    }
+}
